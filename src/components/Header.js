ab60c13a8e1aa38d6e67984bf55ad61a3a10dcfd
Mungo Dewar <mungodewar@skyscanner.net>
2019-03-02 21:15:42 +0000

    Init commit, cloned from gatsby starter

---
+++ src/components/Header.js
@@ -0,0 +1,64 @@
+import React, { Fragment } from 'react';
+import Headroom from 'react-headroom';
+import { Flex } from 'rebass';
+import styled from 'styled-components';
+import { SectionLinks } from 'react-scroll-section';
+import Fade from 'react-reveal/Fade';
+import RouteLink from './RouteLink';
+
+const capitalize = s => s && s[0].toUpperCase() + s.slice(1);
+
+const HeaderContainer = styled(Headroom)`
+  .headroom--pinned {
+    background: ${props => props.theme.colors.primaryDark};
+  }
+
+  position: absolute;
+  width: 100%;
+`;
+
+const formatLinks = allLinks =>
+  Object.entries(allLinks).reduce(
+    (acc, [key, value]) => [...acc, { name: capitalize(key), value }],
+    [],
+  );
+
+const Header = () => (
+  <HeaderContainer>
+    <Fade top>
+      <Flex
+        flexWrap="wrap"
+        justifyContent="space-between"
+        alignItems="center"
+        p={3}
+      >
+        <SectionLinks>
+          {({ allLinks }) => {
+            const links = formatLinks(allLinks);
+
+            const navLinks = links.map(({ name, value }) => (
+              <RouteLink
+                key={name}
+                onClick={value.onClick}
+                selected={value.selected}
+              >
+                {name}
+              </RouteLink>
+            ));
+
+            const [home, ...restLinks] = navLinks;
+
+            return (
+              <Fragment>
+                {home}
+                <Flex mr={[0, 3, 5]}>{restLinks}</Flex>
+              </Fragment>
+            );
+          }}
+        </SectionLinks>
+      </Flex>
+    </Fade>
+  </HeaderContainer>
+);
+
+export default Header;
